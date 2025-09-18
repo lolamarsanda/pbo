8bb11b53b77cb531afe61483cc2b7d936a19055b
lolamarsanda <lolamarsanda117@gmail.com>
2025-09-18 11:33:57 +0700

    Update and rename 02.js to 10.js

---
+++ 10.js
@@ -0,0 +1,10 @@
+const mahasiswa = {
+    nama: "Lola",
+    umur: 21,
+    jurusan: "Informatika"
+};
+
+// Konversi objek ke JSON
+const jsonData = JSON.stringify(mahasiswa);
+fs.writeFileSync('mahasiswa.json', jsonData);
+console.log("Data mahasiswa telah disimpan!");
