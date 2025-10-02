682f558507ff7bc4fbaf0daacc7b616718fd1dde
lolamarsanda <lolamarsanda117@gmail.com>
2025-10-02 12:05:38 +0700

    Update 04.js

--- 04.js
+++ 04.js
@@ -57,24 +57,3 @@ console.log("Nilai setelah diubah prodi3:", prodi3.getNilai()); // tetap 60
   }
 }
 
-class Mahasiswa {
-  constructor(nama, nim, nilai) {
-    this.nama = nama;
-    this.nim = nim;
-
-    let _nilai = nilai;
-
-    this.getNilai = function() {
-      return _nilai;
-    };
-
-    this.setNilai = function(nilaiBaru) {
-      if (nilaiBaru >= 0 && nilaiBaru <= 100) {
-        _nilai = nilaiBaru;
-      } else {
-        console.log('Nilai harus antara 0 dan 100');
-      }
-    };
-  }
-}  // <-- cukup sampai sini saja
-
