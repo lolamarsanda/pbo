dca43ce96f70c8f1010158245eb7c52f52d52010
lolamarsanda <lolamarsanda117@gmail.com>
2025-10-02 12:22:21 +0700

    Update 04.js

--- 04.js
+++ 04.js
@@ -30,17 +30,18 @@ class Mahasiswa {
       if (nilaiBaru >= 0 && nilaiBaru <= 100) {
         _nilai = nilaiBaru;
       } else {
-        console.log("Nilai harus antara 0 dan 100");}
-      };
-    }
+        console.log("Nilai harus antara 0 dan 100");
+      }
+    };
   }
+}
 
-//object Mahasiswa
+// object Mahasiswa
 let prodi1 = new Mahasiswa("Budi", "1234", 85);
 let prodi2 = new Mahasiswa("Ani", "5678", 70);
 let prodi3 = new Mahasiswa("Cici", "9101", 60);
 
-// ---  --- 
+// --- Testing ---
 console.log("Nilai awal prodi1:", prodi1.getNilai()); // 85
 prodi1.setNilai(90);
 console.log("Nilai setelah diubah prodi1:", prodi1.getNilai()); // 90
@@ -52,8 +53,4 @@ console.log("Nilai setelah diubah prodi2:", prodi2.getNilai()); // 95
 console.log("Nilai awal prodi3:", prodi3.getNilai()); // 60
 prodi3.setNilai(110); // salah, akan muncul pesan error
 console.log("Nilai setelah diubah prodi3:", prodi3.getNilai()); // tetap 60
-
-    };
-  }
-}
 
